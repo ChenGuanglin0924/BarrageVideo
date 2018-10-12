354a6b3339bde52cfddf02d3acb8f64893d983fa
ChenGuanglin <diyad7299@qq.com>
2018-10-12 17:38:53 +0800

    【update】基本UI

---
+++ BarrageVideo.js
@@ -0,0 +1,9 @@
+let playTool = document.getElementsByClassName("play-tool")[0],
+    timeCurrent = document.getElementsByClassName("time-current")[0],
+    timeTotal = document.getElementsByClassName("time-total")[0],
+    barrageTool = document.getElementsByClassName("barrage-tool")[0],
+    volumeTool = document.getElementsByClassName("volume-tool")[0],
+    settingTool = document.getElementsByClassName("setting-tool")[0],
+    fullscreenTool = document.getElementsByClassName("fullscreen-tool")[0];
+
+
