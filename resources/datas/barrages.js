06a79ba8342a7664c6ee7180c84d41149f008702
chenguanglin <chenguanglin@supermap.com>
2018-11-21 22:12:48 +0800

    【update】

--- resources/datas/barrages.js
+++ resources/datas/barrages.js
@@ -2,338 +2,107 @@ let barrages = [
     {
         date: "2018-09-24",
         time: "2",
-        text: "我爱你！2",
+        text: "驱蚊器翁呃",
         fontSize: "20",
         color: "red",
         speed: "1"
     },{
         date: "2018-11-02",
         time: "7",
-        text: "我爱你！7",
+        text: "确认挖士大夫萨达",
         fontSize: "20",
         color: "red",
         speed: "2"
     },{
         date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
+        time: "10",
+        text: "是非得失 ",
         fontSize: "20",
         color: "red",
         speed: "3"
     },{
         date: "2018-09-24",
+        time: "14",
+        text: "我爱你发送到发货！30",
+        fontSize: "20",
+        color: "red",
+        speed: "4"
+    },{
+        date: "2018-09-24",
+        time: "18",
+        text: "豆腐干豆腐",
+        fontSize: "20",
+        color: "red",
+        speed: "4"
+    },{
+        date: "2018-09-24",
         time: "20",
-        text: "我爱你！30",
+        text: "结核杆菌",
         fontSize: "20",
         color: "red",
         speed: "4"
     },{
         date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
+        time: "20",
+        text: "是电饭锅水电费特然",
         fontSize: "20",
         color: "red",
         speed: "1"
     },{
         date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
+        time: "20",
+        text: "更舒服的规划投入",
         fontSize: "20",
         color: "red",
         speed: "2"
     },{
         date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
+        time: "25",
+        text: "深V承包人股份",
         fontSize: "20",
         color: "red",
         speed: "3"
     },{
         date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
         time: "40",
-        text: "我爱你！40",
+        text: "删除飋色博尔特 ",
         fontSize: "20",
         color: "red",
         speed: "4"
     },{
         date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
+        time: "35",
+        text: "修订版同居",
         fontSize: "20",
         color: "red",
         speed: "4"
     },{
         date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
+        time: "60",
+        text: "分公司电话图",
+        fontSize: "20",
+        color: "red",
+        speed: "4"
+    },{
+        date: "2018-09-24",
+        time: "129",
+        text: "juj7uiKIKI电饭锅和规范",
         fontSize: "20",
         color: "red",
         speed: "1"
     },{
         date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
+        time: "130",
+        text: "好卡萝莉欧尼 ",
         fontSize: "20",
         color: "red",
         speed: "2"
     },{
         date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
+        time: "150",
+        text: "换个地方很讨厌人家",
         fontSize: "20",
         color: "red",
         speed: "3"
-    },{
-        date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
-        fontSize: "20",
-        color: "red",
-        speed: "1"
-    },{
-        date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
-        fontSize: "20",
-        color: "red",
-        speed: "2"
-    },{
-        date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
-        fontSize: "20",
-        color: "red",
-        speed: "3"
-    },{
-        date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
-        fontSize: "20",
-        color: "red",
-        speed: "1"
-    },{
-        date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
-        fontSize: "20",
-        color: "red",
-        speed: "2"
-    },{
-        date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
-        fontSize: "20",
-        color: "red",
-        speed: "3"
-    },{
-        date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
-        fontSize: "20",
-        color: "red",
-        speed: "1"
-    },{
-        date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
-        fontSize: "20",
-        color: "red",
-        speed: "2"
-    },{
-        date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
-        fontSize: "20",
-        color: "red",
-        speed: "3"
-    },{
-        date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
-        fontSize: "20",
-        color: "red",
-        speed: "1"
-    },{
-        date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
-        fontSize: "20",
-        color: "red",
-        speed: "2"
-    },{
-        date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
-        fontSize: "20",
-        color: "red",
-        speed: "3"
-    },{
-        date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "2",
-        text: "我爱你！2",
-        fontSize: "20",
-        color: "red",
-        speed: "1"
-    },{
-        date: "2018-11-02",
-        time: "7",
-        text: "我爱你！7",
-        fontSize: "20",
-        color: "red",
-        speed: "2"
-    },{
-        date: "2018-09-24",
-        time: "18",
-        text: "我爱你！18",
-        fontSize: "20",
-        color: "red",
-        speed: "3"
-    },{
-        date: "2018-09-24",
-        time: "20",
-        text: "我爱你！30",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "40",
-        text: "我爱你！40",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
-    },{
-        date: "2018-09-24",
-        time: "55",
-        text: "我爱你！55",
-        fontSize: "20",
-        color: "red",
-        speed: "4"
     }
 ]
